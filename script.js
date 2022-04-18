6c14078eb2eeb59c7bf27188303ac85a39dfb618
kamrujama <88245491+kamrujama@users.noreply.github.com>
2022-04-18 23:05:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+let output = document.getElementById("output-screen")
+
+
+
+function isOperator(num){
+    if(num== '+' || num == '-' || num == '*' || num == '/' || num == '%')
+        return true
+    else
+        return false
+}
+
+function display(num){
+    if(output.value === "0"){
+        if(num != '0' && num != "00"){
+            if(num == '.' || isOperator(num)){
+                output.value = output.value + num
+            }
+            else{
+                output.value = num
+            }
+        }
+
+    }
+    else{
+        if(isOperator(output.value[output.value.length - 1]) && isOperator(num)){
+            output.value = output.value.slice(0,-1) + num;
+        }
+        else{
+            output.value += num
+        }
+    }
+}
+
+function delAll(){
+    output.value = "0"
+}
+
+function del(){
+    output.value = output.value.slice(0, -1)
+}
+
+function calculate(){
+    try{
+    output.value = eval(output.value)
+    }
+    catch{
+        alert("Invalid Inputs")
+    }
+}
+
